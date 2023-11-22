2e194d8f46e9cc052e4803151b703d7e27857d13
7shreya <mistryshreya17@gmail.com>
2023-11-22 16:16:13 +0000

    register and login files

---
+++ register.php
@@ -0,0 +1,22 @@
+
+<?php 
+
+//To do:
+//add password complexity requirements
+
+try{
+	
+	#register user by inserting the user info 
+	$stat=$db->prepare("insert into users values(default,?,?,?)");
+	$stat->execute(array($username, $password, $email));
+	
+	$id=$db->lastInsertId();
+	echo "Congratulations! You are now registered. Your ID is: $id  ";  	
+	
+ }
+ catch (PDOexception $ex){
+	echo "Sorry, a database error occurred! <br>";
+	echo "Error details: <em>". $ex->getMessage()."</em>";
+ }
+
+ ?>
