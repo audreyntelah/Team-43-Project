c0d6300134a43701414fe729065dbe7ee28b49e5
NJG63 <147707968+NJG63@users.noreply.github.com>
2023-12-01 00:44:48 +0000

    Added the text to the website for the products on the home page

--- Home_Page.js
+++ Home_Page.js
@@ -1,12 +1,13 @@
 
 	window.onscroll = function() {scrollFunction()};
 	// This function is necessary becuase we need to create a point where the user scrolls down past a point so the logo can "disappear" and "reappear"
-	function scrollFunction() {
+	//The Scroll Function is triggered when the user scrolls past a certain point , then it will adjust the styling on the logo div and the logo image itself.This creates a animation 
+    function scrollFunction() {
 	  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 	    document.getElementById("logo_div").style.padding ="10px 5px";
 	    
-	    document.getElementById("logo").style.width ="0%";
-	    document.getElementById("logo").style.height ="0%";
+	    document.getElementById("logo").style.width ="5%";
+	    document.getElementById("logo").style.height ="5%";
 
 
 
@@ -23,31 +24,30 @@
 		let slideIndex = 0;
 		showSlides();
 		//This function is required so that the slideshow can automatically move along and have the indicators follow in suit 
-		function showSlides() {
-		  let i;
-		  let slides = document.getElementsByClassName("mySlides");
-		  let dots = document.getElementsByClassName("Slide_Indicator");
-		  for (i = 0; i < slides.length; i++) {
-		    slides[i].style.display = "none";  
-		  }
-		  slideIndex++;
-		  if (slideIndex > slides.length) {slideIndex = 1}    
-		  for (i = 0; i < dots.length; i++) {
-		    dots[i].className = dots[i].className.replace(" active", "");
-		  }
-		  slides[slideIndex-1].style.display = "block";  
-		  dots[slideIndex-1].className += " active";
-		  setTimeout(showSlides, 10000); 
-		}
+	function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("Slide_Indicator");
+        for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+        setTimeout(showSlides, 10000); 
+        }
 
 
         
     //This function has been designed to display a modal window the first time a user visits a webpage and then hide it on subsequent visits. It accomplishes this by using cookies to keep track of whether the modal has been shown before .
 	        
 	window.addEventListener('load', function() {
-	    var modal = document.getElementById('my_Modal');
+	    var modal = document.getElementById('Login_Modal');
 	    var closeModal = document.getElementById('close_Modal');
-
 	    var modalShown = getCookie('modalShown');
 
 	    if (!modalShown) {
@@ -60,6 +60,7 @@
 	    }
 	});
 
+    //Sets a cookie with a given name, value and expiration time 
 	function setCookie(name, value, days) {
 	    var expires = '';
 	    if (days) {
@@ -69,7 +70,7 @@
 	    }
 	    document.cookie = name + '=' + value + expires + '; path=/';
 	}
-
+    //Retrieves the value of a cookie by name 
 	function getCookie(name) {
 	    var nameEQ = name + '=';
 	    var cookies = document.cookie.split(';');
@@ -85,7 +86,7 @@
 	    return null;
 	}
 
-
+        // Sets up the Owl Carousel for the image carousel
     	var owl = $('.owl-carousel');
         
         function pauseCarousel() {
@@ -98,12 +99,12 @@
 
         owl.owlCarousel({
             loop: true,
-            margin: 10,
+            margin: 0,
             nav: false,
             autoplay: true,
             dots: false,
             autoplayTimeout: 1000,
-            padding: 50,
+            padding: 0,
             responsive: {
                 0: {
                     items: 1
@@ -117,81 +118,158 @@
             }
         });
 
+        // ShowRegisterForm , ShowAdminForm ,ShowLoginForm these dynamically change the modal in realtime 
     	//This function is needed to change the login modal so that other forms can be displayed 
-        function showRegisterForm() {
-            // Get the form element
-            const modalForm = document.getElementById('modalForm');
+    function showRegisterForm() {
+        // Get the form element
+        const modalForm = document.getElementById('modalForm');
 
-            // Change the form content
-            modalForm.innerHTML = `
-                <button class="Register_button" onclick="showLoginForm()">Back to Login</button>
-                <span class="close" id="close_Modal" onclick="closeModal()">&times;</span>
-                <div class="model_img_container">
-                    <a href="javascript:showAdminForm()"><img src="logo.png" class="Login_img"></a>
-                </div>
-                <div class="username_password_container">
-                    <label for="newUsername"><h2>Create a Username</h2></label>
-                    <input type="username" placeholder="Enter a Username" name="newUsername">
-                    <label for="newPassword"><b><h2>New Password</h2></b></label>
-                    <input type="password" placeholder="Enter a Password" name="newPassword">
-                    <button class="Login_submit_button" type="submit">Register</button>
-                </div>
-            `;
-        }
+        // Change the form content
+        modalForm.innerHTML = `
+            <button class="Register_button" onclick="showLoginForm()">Back to Login</button>
+            <span class="close" id="close_Modal" onclick="closeModal()">&times;</span>
+            <div class="model_img_container">
+                <a href="javascript:showAdminForm()"><img src="logo.png" class="Login_img"></a>
+            </div>
+            <div class="username_password_container">
+                <label class="Label" for="newUsername"><b><p> Create a Username</b></p></label>
+                <input type="username" placeholder="Enter a Username" name="newUsername">
+                <label class="Label" for="newPassword"><b><p>Create a Password</p></b</label>
+                <input type="password" placeholder="Enter a Password" name="newPassword">
+                <button class="Login_submit_button" type="submit"><b>Register</b></button>
+            </div>
+        `;
 
-        function showLoginForm() {
-            
-            const modalForm = document.getElementById('modalForm');
-            modalForm.innerHTML = `
-                <button class="Register_button" onclick="showRegisterForm()">Register</button>
-                <span class="close" id="close_Modal"onclick="closeModal()">&times;</span>
-                <div class="model_img_container">
-                    <a href="javascript:showAdminForm()"><img src="logo.png" class="Login_img"></a>
-                </div>
-                <div class="username_password_container">
-                    <label for="uname"><h2>Username</h2></label>
-                    <input type="username" placeholder="Enter Username" name="uname">
-                    <label for="psw"><b><h2>Password</h2></b></label>
-                    <input type="password" placeholder="Enter Password" name="psw">
-                    <button class="Login_submit_button" type="submit">Login</button>
-                    <div class="username_password_footer">
-                        <label>
-                            <input type="checkbox" unchecked="unchecked" name="remember"> Remember me
-                        </label>
-                        <br><br>
-                        <span><a href="javascript:alert('We have sent a password reset email to your email address , please check your inbox');" style="color: white;">Forgot Password?</a></span>
-                    </div>
-                </div>
-            `;
-        }
-        function showAdminForm(){
-        	const modalForm = document.getElementById('modalForm');
-            modalForm.innerHTML = `
-                <button class="Register_button" onclick="showLoginForm()">Customer Login</button>
-                <span class="close" id="close_Modal"onclick="closeModal()">&times;</span>
-                <div class="model_img_container">
-                    <a href="javascript:showAdminForm()"><img src="logo.png" class="Login_img"></a>
-                </div>
-                <div class="username_password_container">
-                    <label for="uname"><h2>Admin Username</h2></label>
-                    <input type="username" placeholder="Enter Admin Username" name="uname">
-                    <label for="psw"><b><h2>Admin Password</h2></b></label>
-                    <input type="password" placeholder="Enter Admin Password" name="psw">
-                    <button class="Login_submit_button" type="submit">Login</button>
-                    <div class="username_password_footer">
-                    <br>
-                        <span><a href="javascript:alert('We have sent a password reset email to your email address , please check your inbox');" style="color: white;">Forgot Password?</a></span>
-                    </div>
-                </div>
-            `;
-        }
+       
 
-        function closeModal() {
-            const openModal = document.querySelector('.login_modal[style="display: block;"]');
-            if (openModal) {
-                openModal.style.display = 'none';
-                }
+    }
+
+    function showLoginForm() {
+        
+        const modalForm = document.getElementById('modalForm');
+        modalForm.innerHTML = `
+            <button class="Register_button" onclick="showRegisterForm()">Register Here</button>
+            <span class="close" id="close_Modal"onclick="closeModal()">&times;</span>
+            <div class="model_img_container">
+                <a href="javascript:showAdminForm()"><img src="logo.png" class="Login_img"></a>
+            </div>
+            <div class="username_password_container">
+                <label class="Label" for="uname"><b><p>Username</p></b></label>
+                <input type="username" placeholder="Enter Username" name="uname">
+                <label class="Label" for="psw"><b><p>Password</p></b></label>
+                <input type="password" placeholder="Enter Password" name="psw">
+                <button class="Login_submit_button" type="submit"><b>Login</b></button>
+                <div class="username_password_footer">
+                    <label>
+                        <input type="checkbox" unchecked="unchecked" name="remember"><b> Remember me</b>
+                    </label>
+                    <br><br>
+                    <span><a href="javascript:alert('We have sent a password reset email to your email address , please check your inbox');" style="color: white;">Change Password</a></span>
+                </div>
+            </div>
+        `;
+    }
+    function showAdminForm(){
+    	const modalForm = document.getElementById('modalForm');
+        modalForm.innerHTML = `
+            <button class="Register_button" onclick="showLoginForm()">Customer Login</button>
+            <span class="close" id="close_Modal"onclick="closeModal()">&times;</span>
+            <div class="model_img_container">
+                <a href="javascript:showAdminForm()"><img src="logo.png" class="Login_img"></a>
+            </div>
+            <div class="username_password_container">
+                <label class="Label" for="uname"><b><p>Admin Username<b></p></label>
+                <input type="username" placeholder="Enter Admin Username" name="uname">
+                <label class="Label" for="psw"><b><p>Admin Password</p></b></label>
+                <input type="password" placeholder="Enter Admin Password" name="psw">
+                <button class="Login_submit_button" type="submit"><b>Login</b></button>
+                <div class="username_password_footer">
+                <span><a href="javascript:alert('We have sent a password reset email to your email address , please check your inbox');" style="color: white;">Change Password</a></span>
+                </div>
+            </div>
+        `;
+    }
+    // Closes the login modal
+    function closeModal() {
+        const openModal = document.querySelector('.login_modal[style="display: block;"]');
+        if (openModal) {
+            openModal.style.display = 'none';
             }
+        }
+    //Changes the styling of a section when the mouse is hovers over it 
+    function Colour_Change_Left() {
+        var elementToChange = document.querySelector('.section_left_container');
+        var elementToChange2= document.querySelector('.section_left_container_text');
+        var elementToChange3= document.querySelector('.section_left_button');
+        elementToChange.style.backgroundColor = 'darkgray';
+        elementToChange2.style.color ='black';
+        elementToChange.style.borderColor='6px black';
+        elementToChange.style.borderStyle='dashed';
 
-     
-    
+
+
+    }
+
+    function Colour_Restore_Left() {
+        var elementToChange = document.querySelector('.section_left_container');
+        var elementToChange2= document.querySelector('.section_left_container_text');
+        var elementToChange3= document.querySelector('.section_left_button');
+        elementToChange.style.backgroundColor = 'black';
+        elementToChange2.style.color ='white';
+        elementToChange3.style.backgroundColor='black';
+        elementToChange3.style.color='white';
+        elementToChange3.style.borderColor='white';
+        elementToChange.style.borderColor='none';
+        elementToChange.style.borderStyle='none';
+
+
+
+    }
+
+    function Colour_Change_Right() {
+        var elementToChange = document.querySelector('.section_right_container');
+        var elementToChange2= document.querySelector('.section_right_container_text');
+        var elementToChange3= document.querySelector('.section_right_button');
+        elementToChange.style.backgroundColor = 'darkgray';
+        elementToChange2.style.color ='black';
+        elementToChange.style.borderColor='6px black';
+        elementToChange.style.borderStyle='dashed';
+
+
+
+    }
+
+    function Colour_Restore_Right() {
+        var elementToChange = document.querySelector('.section_right_container');
+        var elementToChange2= document.querySelector('.section_right_container_text');
+        var elementToChange3= document.querySelector('.section_right_button');
+        elementToChange.style.backgroundColor = 'black';
+        elementToChange2.style.color ='white';
+        elementToChange3.style.backgroundColor='black';
+        elementToChange3.style.color='white';
+        elementToChange3.style.borderColor='white';        
+        elementToChange.style.borderColor='none';
+        elementToChange.style.borderStyle='none';
+
+
+
+    }
+    //Opens up the login modal when a button is pressed and sets up to needed behaviours 
+    function Open_Login_Modal(){
+        var modal = document.getElementById('Login_Modal');
+        var closeModal = document.getElementById('close_Modal');
+        var modalShown = getCookie('modalShown');
+
+        if (!modalShown) {
+            modal.style.display = 'block';
+            showLoginForm();
+
+            closeModal.addEventListener('click', function() {
+            
+                modal.style.display = 'none';
+                setCookie('modalShown', 'true', 30);
+            });
+        }
+    }
+
+
